db3aae7d10f51637c8bf62d6067e5f248baa10f6
Luca Ramundo <0xc14m1z@gmail.com>
2018-10-23 00:01:59 +0200

    Add store enhancer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
+import { createStore, applyMiddleware } from 'redux'
+
 const middleware = socket => store => next => action => {
   socket.emit(action.type, action)
   return next(action)
 }
 
-export default middleware
-module.exports = middleware
+const listenForRemoteActions = (socket, remoteActionType) => store =>
+  socket.on(remoteActionType, action => store.dispatch(action))
+
+const enhancer = (socket, remoteActionType) => createStore => (...args) => {
+  const store = createStore(...args, applyMiddleware(middleware(socket)))
+
+  listenForRemoteActions(socket, remoteActionType)
+
+  return store
+}
+
+const reduxSocketDispatch = {
+  socketDispatchMiddleware: middleware,
+  socketDispatchEnhancer: enhancer
+}
+
+export default reduxSocketDispatch
+module.exports = reduxSocketDispatch

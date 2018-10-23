189e6447f71b514c628e0c7b8fad349fa509182b
Luca Ramundo <0xc14m1z@gmail.com>
2018-10-23 08:56:20 +0200

    Avoid re-dispatching a remote action back to the server

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import { applyMiddleware } from 'redux'
 
+// take a socket and return a middleware that emits an event
+// through the socket of type `action.type` and the entire action as payload
 const middleware = socket => store => next => action => {
-  socket.emit(action.type, action)
+  // avoid emitting actions marked as dispatched from the server
+  if ( !action.__remote__ ) socket.emit(action.type, action)
   return next(action)
 }
 
-const listenForRemoteActions = (socket, remoteActionType, store) =>
-  socket.on(remoteActionType, store.dispatch)
+// add an handler to the socket for the given remote action type and dispatch
+// the payload action marking it as remote (in order to avoid re-dispatching)
+const listenForRemoteActions = (socket, remoteActionType, store) => {
+  socket.on(remoteActionType, action => {
+    store.dispatch({ ...action, __remote__: true })
+  })
+}
 
+// take a socket and a remote action type and return a redux store enhancer
 const enhancer = (socket, remoteActionType) => createStore => (...args) => {
-  const store = createStore(...args, applyMiddleware(middleware(socket)))
+  const socketMiddleware = middleware(socket)
+  const store = createStore(...args, applyMiddleware(socketMiddleware))
 
   listenForRemoteActions(socket, remoteActionType, store)
 
